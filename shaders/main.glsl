38695c3993fded0a820016cd21775cab88da241a
w0nyv <kani.king241@gmail.com>
2025-08-08 00:23:46 +0900

    とりあえず 16 に達した

--- shaders/main.glsl
+++ shaders/main.glsl
@@ -6,11 +6,10 @@ out vec4 color;
 
 float m001(vec2 p) {
     float t = floor(beat) + easeOutExpo(fract(beat));
-
     float c = cyclic(vec3(p * 0.7, beat * 0.3 + t * 0.6), 2.0).r;
     c = 0.9 - abs(c);
     c *= c * c * c;
-    
+    c *= 1.35;
     return c;
 }
 
@@ -23,7 +22,7 @@ float m002() {
     
     float t = easeOutExpo(fract(beat));
     
-    float f = step(length(fp), t * 0.1);
+    float f = step(length(fp), 0.065 + t * 0.035);
     
     return f;
 }
@@ -126,6 +125,109 @@ float m007() {
     return pow(sin(sdBox(fp, vec2(1.0)) * 10.0 - beat * 4.0), 3.0) * step((length(fp.x)), 0.4);
 }
 
+float m008(vec2 p) {
+    float o = floor(gl_FragCoord.x / resolution.x * 7.0)/7.0 * acos(-1.0) * 2.0;
+    return clamp(pow(abs(sin(-p.y - beat / 2.0 + o)), 8.0) + 0.1, 0.0, 1.0);
+}
+
+float m009(vec2 p) {
+    
+    float t = floor(beat) + easeOutElastic(fract(beat));
+    
+    float cx = cyclic(vec3(p * 0.5, t * 0.45), 2.0).r;
+    float cy = cyclic(vec3(p.y, t * 0.45, p.x * 2.0), 2.0).r;
+    
+    vec2 p2 = vec2(cx, cy);
+    p2 *= rot(-beat * 0.2);
+
+    float c = cyclic(vec3(p2.y * 10.0, t * 0.3, p2.x * 10.0), 10.0).x;
+    
+    return pow(sin(c * 2.0), 3.0);
+}
+
+float m010() {
+    vec2 uv = (gl_FragCoord.xy / resolution.xy);
+
+    vec2 fp = vec2(fract(uv.x * 7.0), uv.y) - 0.5;
+    float f = 0.0;
+    
+    for (float i = -0.2; i <= 0.2; i += 0.2)
+    {
+        vec2 p2 = fp;
+        p2.x += cyclic(vec3(p2.y, floor(uv.x*7.0)-0.5 + i, floor(beat*8.0)), 1.5).r * 0.2;
+        f += step(length(p2.x - i), 0.02);
+    }
+    
+    return min(f, 1.0);
+}
+
+float m011() {
+    vec2 uv = (gl_FragCoord.xy / resolution.xy);
+    return sin(acos(-1.0) * uv.x * 7.0 -  beat * acos(-1.0));
+}
+
+float m012(vec2 p) {
+    p *= 0.3;
+    
+    float c = sin(p.x * 5.0);
+    c += sin((p.y * 10.0 + beat)/2.0);
+    c += sin((p.x * 2.0 + p.y * 20.0)/2.0);
+    c = sin(c * 5.0 + beat * 2.0);
+    c += cos(p.x*10.0)*sin(p.x*10.0 + beat);
+    c = cos(c * 3.0 + beat * 2.0 + p.y * 20.0);
+    
+    return c;
+}
+
+float m013(vec2 p) {
+    
+    float f = 0.0;
+    
+    for (float i = 1.0; i < 6.0; i += 1.0) {
+        
+        vec2 p2 = p;
+        p2.x += beat / i * 0.25;
+        p2 = fract(p2 * i * 3.5 / 2.0) - 0.5;
+        
+        f += 0.02 / length(p2);
+        f /= i * 0.3;
+    }
+    
+    return min(pow(f, 2.0), 1.0);
+}
+
+float m014(vec2 p) {
+    float bounce = floor(beat) + easeOutExpo(fract(beat));
+    vec3 noise = cyclic(vec3(beat/2.0, 324.32, 745.43), 8.0) * 0.5 + vec3(1.0);
+    float id = floor(p.x * (0.2 + noise.x * 2.0) + beat / 2.0) - 0.5;
+    p.x = fract(p.x * (0.2 + noise.x * 2.0) + beat / 2.0) - 0.5;
+    float d = cyclic(vec3(beat / 4.0 + bounce * 0.65, id, 2312.32), 8.0).x;
+    float f = length(p.x + asin(sin(bounce * 0.75 + p.y * (2.0 + d * 8.0))) * 0.2);
+    f = step(f, 0.01 + 0.1 * (cyclic(vec3(id), 2.0).y * 0.5 + 1.0));
+    return f;
+}
+
+float m015() {
+    vec2 uv = (gl_FragCoord.xy / resolution.xy);
+    vec3 rnd = hash(vec3(floor(uv.x*7.0)-0.5, 4323.2, 125.34));
+    vec2 fp = vec2(fract(uv.x*7.0), uv.y) - 0.5;
+    fp.y -= 0.085;
+    float t = beat + (floor(uv.x*7.0)-0.5)/(3.5/2.0);
+    float d = step(sdBox(fp, vec2(easeInExpo(1.5, fract(t*0.25)))), 0.0);
+    d = mod(floor(t), 8.0) > 3.0 ? d : 1.0 - d;
+    return d;
+}
+
+float m016() {
+    vec2 uv = (gl_FragCoord.xy / resolution.xy);
+    vec3 rnd = hash(vec3(floor(uv.x*7.0)-0.5, 4323.2, 125.34));
+    vec2 fp = vec2(fract(uv.x*7.0), uv.y) - 0.5;
+    float t = beat + (floor(uv.x*7.0)-0.5)/(7.0/32.0);
+    float d = step(sdBox(fp, vec2(easeInElastic(fract(-t/8.0)), 1.0)), 0.0);
+    d = mod(floor(-t), 16.0) > 7.0 ? d : 1.0 - d;
+    return d;
+}
+
 void main() {
     
     vec2 p = (gl_FragCoord.xy * 2.0 - resolution.xy) / min(resolution.x, resolution.y);
@@ -133,7 +235,7 @@ void main() {
     
     int offset = 16;
     int minIdx = offset;
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 16; i++)
     {
         if (buttons[i + offset].y < buttons[minIdx].y)
         {
@@ -148,8 +250,17 @@ void main() {
     dest = minIdx == offset + 4 ? m005(p) : dest;
     dest = minIdx == offset + 5 ? m006() : dest;
     dest = minIdx == offset + 6 ? m007() : dest;
+    dest = minIdx == offset + 7 ? m008(p) : dest;
+    dest = minIdx == offset + 8 ? m009(p) : dest;
+    dest = minIdx == offset + 9 ? m010() : dest;
+    dest = minIdx == offset + 10 ? m011() : dest;
+    dest = minIdx == offset + 11 ? m012(p) : dest;
+    dest = minIdx == offset + 12 ? m013(p) : dest;
+    dest = minIdx == offset + 13 ? m014(p) : dest;
+    dest = minIdx == offset + 14 ? m015() : dest;
+    dest = minIdx == offset + 15 ? m016() : dest;
 
-    vec3 col = vec3(0.1, 0.8, 0.5) * dest;
+    vec3 col = vec3(0.1, 0.4, 1.0) * dest;
     
     color = vec4(col, 1.0);
 }
--- shaders/mainPostEffect.glsl
+++ shaders/mainPostEffect.glsl
@@ -8,6 +8,15 @@ uniform sampler2D sobelTex;
 
 out vec4 color;
 
+float edge(vec2 uv)
+{
+    uv.x += 0.5;
+    uv.y -= 0.585;
+    vec2 fp = vec2(fract(uv.x * 7.0), fract(uv.y*1.2)) - 0.5;
+    
+    return clamp(step(length(fp.x), 0.04*0.75) + step(length(fp.y), 0.01), 0.0, 1.0);
+}
+
 void main() {
 
     vec2 uv = (gl_FragCoord.xy / resolution.xy);
@@ -16,6 +25,7 @@ void main() {
 
     vec4 main = texture(mainTex, uv);
     vec4 sobel = texture(sobelTex, uv + noise.xy * 0.015);
-
-    color = main + sobel;
+    vec4 edge = vec4(edge(uv)) * abs(sin(beat*acos(-1.0)/8.0));
+    
+    color = main + sobel + edge;
 }
